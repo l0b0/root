2ad3b8085776417a0e49a401373154d04ebc4190
Victor Engmark <victor.engmark@gmail.com>
2014-11-06 20:56:23 +0000

    Specify puppet-lint version

--- test/install-puppet.sh
+++ test/install-puppet.sh
@@ -1,2 +1,2 @@
 pacman --sync --needed --refresh --noconfirm puppet
-sudo -u vagrant gem install --no-document --user-install puppet-lint
+sudo -u vagrant gem install --version 1.1.0  --no-document --user-install puppet-lint
