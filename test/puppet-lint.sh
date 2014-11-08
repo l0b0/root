cb81d46ab82077ddb50da37a9d26f9e17dd2f42f
Victor Engmark <victor.engmark@gmail.com>
2014-11-08 18:19:51 +0000

    Make `puppet-lint` fail on warnings

--- test/puppet-lint.sh
+++ test/puppet-lint.sh
@@ -2,4 +2,4 @@
 find /vagrant/manifests /vagrant/modules \
     \( -path /vagrant/modules/stdlib -o -path /vagrant/modules/ufw \) -prune -false -o \
     -type f -name '*.pp' \
-    -exec ~/.gem/ruby/*/gems/puppet-lint-*/bin/puppet-lint --no-documentation-check {} +
+    -exec ~/.gem/ruby/*/gems/puppet-lint-*/bin/puppet-lint --fail-on-warnings --no-documentation-check {} +
