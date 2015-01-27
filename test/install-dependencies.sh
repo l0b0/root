98e01902482042f717647befc7db2e00fe10b8ca
Victor Engmark <victor.engmark@gmail.com>
2015-01-27 11:47:43 +0000

    Upgrade Ruby before running Puppet
    Fixes issue #3 <https://github.com/l0b0/root/issues/3>.

--- test/install-dependencies.sh
+++ test/install-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pacman --sync --needed --refresh --noconfirm puppet ruby-shadow
+pacman --sync --needed --refresh --noconfirm puppet ruby ruby-shadow
 sudo --user=vagrant gem install --version 1.1.0 --no-document --user-install puppet-lint
 sudo --user=vagrant gem install --version 1.3.8 --no-document --user-install reek
 rm -f ~root/.profile ~vagrant/.profile
