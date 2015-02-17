f80df35934f4166597872bcef738817bbd214770
Victor Engmark <victor.engmark@gmail.com>
2015-02-17 19:27:22 +0000

    Replace multiple LibreOffice packages with a single one

--- modules/office_suite/manifests/init.pp
+++ modules/office_suite/manifests/init.pp
@@ -1,11 +1,7 @@
 class office_suite {
   include window_manager
 
-  package {
-    [
-      'libreoffice-still-calc',
-      'libreoffice-still-impress',
-      'libreoffice-still-writer']:
+  package { 'libreoffice-still':
     ensure => latest,
   }
 }
