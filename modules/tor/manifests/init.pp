3d0c031cfde65b31eda58d58599ff74c60bcaf68
Victor Engmark <victor.engmark@gmail.com>
2015-03-02 14:15:31 +0000

    Add default Tor service configuration

--- modules/tor/manifests/init.pp
+++ modules/tor/manifests/init.pp
@@ -2,6 +2,10 @@ class tor {
   package { 'torsocks':
     ensure => latest,
   }->
+  file { '/etc/tor/torrc':
+    ensure => present,
+    source => 'puppet:///modules/tor/torrc';
+  }->
   service { 'tor':
     ensure => running,
     enable => true,
