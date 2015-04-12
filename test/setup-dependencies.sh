779469320aacf9aa12c28dcf40c8f5551bc0448c
Victor Engmark <victor.engmark@gmail.com>
2015-04-12 12:06:42 +0100

    Refresh keys before doing a full system upgrade
    Otherwise some packages will not successfully upgrade.

--- test/setup-dependencies.sh
+++ test/setup-dependencies.sh
@@ -3,6 +3,7 @@ set -o errexit -o xtrace
 pacman --remove --noconfirm ack bash-completion cloc colordiff cowsay dd_rescue ddrescue dmidecode dnsutils dstat \
     ethtool figlet fortune-mod gdb glances htop hwinfo inxi lshw lsof nethogs ngrep nmap ponysay pv ranger reptyr sl \
     smem sshfs stow strace tmux yaourt zsh
+pacman-key --refresh-keys
 pacman --sync --refresh --sysupgrade --noconfirm
 pacman-db-upgrade
 pacman --sync --needed --refresh --noconfirm puppet ruby ruby-shadow
