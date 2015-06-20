abc6f22d07c2144f1467312f317067c71baec125
Victor Engmark <victor.engmark@gmail.com>
2015-06-20 15:36:12 +0100

    Split excluded paths into lines

--- test/puppet-lint.sh
+++ test/puppet-lint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 find /vagrant/manifests /vagrant/modules \
-    \( -path /vagrant/modules/stdlib -o -path /vagrant/modules/ufw \) -prune -false -o \
+    \( \
+        -path /vagrant/modules/stdlib -o \
+        -path /vagrant/modules/ufw \
+    \) -prune -false -o \
     -type f -name '*.pp' \
     -exec puppet-lint --fail-on-warnings --no-documentation-check {} +
