007649a45212696681f63c0fe2aab1009626b79c
Victor Engmark <victor.engmark@gmail.com>
2015-06-20 17:04:41 +0100

    Split excluded paths into lines

--- test/reek.sh
+++ test/reek.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 find /vagrant \
-    \( -path /vagrant/modules/stdlib -o -path /vagrant/modules/ufw \) -prune -false -o \
+    \( \
+        -path /vagrant/modules/stdlib -o \
+        -path /vagrant/modules/ufw \
+    \) -prune -false -o \
     -type f \( -name '*.rb' -o -name Vagrantfile \) \
     -exec reek --verbose {} +
