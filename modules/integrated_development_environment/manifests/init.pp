449e6c8a681cc26cde74002692183acc1a2f518f
Victor Engmark <victor.engmark@gmail.com>
2015-06-30 08:47:04 +0100

    Print instructions to create IDEA command-line launcher

--- modules/integrated_development_environment/manifests/init.pp
+++ modules/integrated_development_environment/manifests/init.pp
@@ -2,9 +2,12 @@ class integrated_development_environment {
   include window_manager
   include java_development_kit
 
+  $version = '14.1.4'
+
   class { 'archive::prerequisites': }
   ->
   class { 'idea::ultimate':
-    version => '14.1.4',
-  }
+    version => $version,
+  }->
+  notify { "Run `JAVA_HOME=/usr/lib/jvm/*-jdk /opt/idea-${version}/idea-IU-*/bin/idea.sh` and use Tools -> Create Command-line Launcher…": }
 }
