55c286877db40cbab0ed62a3b9b769d2dbdfddd2
Victor Engmark <victor.engmark@gmail.com>
2015-07-04 12:39:29 +0100

    Fix setting OpenJDK 8 as the default Java

--- modules/java_development_kit/manifests/init.pp
+++ modules/java_development_kit/manifests/init.pp
@@ -2,7 +2,7 @@ class java_development_kit {
   package { ['jdk8-openjdk']:
     ensure => latest,
   }
-  exec { '/usr/bin/archlinux-java set java-8-jdk':
+  exec { '/usr/bin/archlinux-java set java-8-openjdk':
     subscribe => Package['jdk8-openjdk'],
   }
 }
