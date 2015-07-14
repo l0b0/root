a1fe36909c6e77c1391a3643c4f549879fd291c0
Victor Engmark <victor.engmark@gmail.com>
2015-07-14 19:40:22 +0100

    Remove foomatic and related packages
    After doing this `system-config-printer`
    <https://bugs.archlinux.org/task/43708> is able to find and install drivers
    for Wi-Fi printers.

--- modules/printing_system/manifests/init.pp
+++ modules/printing_system/manifests/init.pp
@@ -2,6 +2,9 @@ class printing_system {
   package { ['cups', 'cups-filters']:
     ensure => latest,
   }->
+  package { ['foomatic-db', 'foomatic-db-engine', 'hplip']:
+    ensure => absent,
+  }
   service { 'org.cups.cupsd':
     ensure => running,
     enable => true,
