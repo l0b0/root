7f91c2a64136cbc1d69fac0f926218caf8ac3933
Victor Engmark <victor.engmark@gmail.com>
2015-10-15 21:32:27 +0100

    Simplify configuration to service relationship dependency ordering

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -1,12 +1,10 @@
 class sshd {
-  service { 'sshd':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/ssh/sshd_config'],
-  }
-
   file { '/etc/ssh/sshd_config':
     ensure  => present,
     content => template('sshd/sshd_config.erb'),
+  }~>
+  service { 'sshd':
+    ensure => running,
+    enable => true,
   }
 }
