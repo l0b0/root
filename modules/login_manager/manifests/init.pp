fc40f92ed2073049fc9dadda7aeb538dc9050602
Victor Engmark <victor.engmark@gmail.com>
2015-10-15 21:14:23 +0100

    Remove reference to long-removed Slim login manager

--- modules/login_manager/manifests/init.pp
+++ modules/login_manager/manifests/init.pp
@@ -1,16 +1,10 @@
 class login_manager {
   include display_server
 
-  service { 'slim':
-    enable => false,
-  }->
   package { ['lightdm', 'lightdm-gtk-greeter']:
     ensure => latest,
   }->
   service { 'lightdm':
     enable => true,
-  }->
-  package { 'slim':
-    ensure => absent,
   }
 }
