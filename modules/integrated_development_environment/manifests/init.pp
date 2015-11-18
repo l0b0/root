fe01f79ce42f712d2e7a8f963a45e12670d9f2a7
Victor Engmark <victor.engmark@gmail.com>
2015-11-18 19:50:37 +0000

    Update IDEA

--- modules/integrated_development_environment/manifests/init.pp
+++ modules/integrated_development_environment/manifests/init.pp
@@ -5,8 +5,8 @@ class integrated_development_environment {
   class { 'archive::prerequisites': }
   ->
   class { 'idea::ultimate':
-    version => '15.0',
-    build   => '143.381.42',
+    version => '15.0.1',
+    build   => '143.382.35',
     timeout => 600,
   }->
   file { '/usr/local/bin/idea':
