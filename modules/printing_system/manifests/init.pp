6159d9a14a402e41612d85163256bb3ce7983a2d
Victor Engmark <victor.engmark@gmail.com>
2015-12-03 20:04:16 +0000

    Remove reference to non-existing CUPS browse daemon on Debian

--- modules/printing_system/manifests/init.pp
+++ modules/printing_system/manifests/init.pp
@@ -1,6 +1,6 @@
 class printing_system (
-  $browser_service,
   $print_service,
+  $browser_service = undef,
 ) {
   include service_discovery_system
 
@@ -23,9 +23,14 @@ class printing_system (
       ensure    => running,
       enable    => true,
       subscribe => [File[$paper_size_file], Package['cups']];
-    $browser_service:
-      ensure    => running,
-      enable    => true,
-      subscribe => Package['cups'];
+  }
+
+  if ($browser_service != undef) {
+    service {
+      $browser_service:
+        ensure    => running,
+        enable    => true,
+        subscribe => Package['cups'];
+    }
   }
 }
