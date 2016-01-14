f05acd2dc3501accfa5653e5035a89e79fa34e6f
Victor Engmark <victor.engmark@gmail.com>
2016-01-14 18:13:51 +0000

    Prevent exposure to SSH client CVE-2016-0777
    <https://access.redhat.com/articles/2123781>.

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -1,9 +1,13 @@
 class sshd (
   $service_name,
 ) {
-  file { '/etc/ssh/sshd_config':
-    ensure  => present,
-    content => template('sshd/sshd_config.erb'),
+  file {
+    '/etc/ssh/sshd_config':
+      ensure  => present,
+      content => template('sshd/sshd_config.erb');
+    '/etc/ssh/ssh_config':
+      ensure => present,
+      source => 'puppet:///modules/sshd/ssh_config'
   }~>
   service { $service_name:
     ensure => running,
