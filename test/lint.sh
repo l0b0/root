885a65ba44ad9c14ffb8e21d520fcf2e96afdb43
Victor Engmark <victor.engmark@gmail.com>
2016-02-19 20:26:47 +0000

    Set the Gem path to the Gem user directory
    This avoids using RVM on Travis CI.

--- test/lint.sh
+++ test/lint.sh
@@ -2,9 +2,8 @@
 set -o errexit -o noclobber -o nounset -o xtrace
 
 dir=/vagrant/test
-gem_user_dir="$(ruby -e 'print Gem.user_dir')"
-
-export PATH="${gem_user_dir}/bin:$PATH"
+export GEM_PATH="$(ruby -e 'print Gem.user_dir')"
+export PATH="${GEM_PATH}/bin:$PATH"
 
 "${dir}/puppet-lint.sh"
 "${dir}/reek.sh"
