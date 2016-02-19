621aaa8a581ff9cfb26decfddd6d13ab040de3ca
Victor Engmark <victor.engmark@gmail.com>
2016-02-19 20:04:36 +0000

    Don't install the netctl-auto@interface service
    Need to solve how to get the interface name first:
    <https://ask.puppetlabs.com/question/24252/how-to-get-a-list-of-wireless-interfaces/>.

--- modules/network_manager/manifests/init.pp
+++ modules/network_manager/manifests/init.pp
@@ -5,25 +5,11 @@ class network_manager (
   if ($packages != undef) {
     include shell
 
-    $service_name = 'netctl-auto@wlp1s0.service'
-    $service_conflict_warning = join(
-      [
-        "Network manager service ${service_name} is running.",
-        'Make sure to stop any other network managers to avoid conflict!',
-      ],
-      ' ')
-
     package { $packages:
       ensure => latest,
-    }->
-    service { $service_name:
-      ensure => running,
-      enable => true,
     }->
     package { $old_packages:
       ensure => absent,
     }
-
-    warning($service_conflict_warning)
   }
 }
