ffc244195a962e56f00379c46dd1c8763084e83a
Victor Engmark <victor.engmark@gmail.com>
2016-04-16 01:10:26 +0100

    Try to run gem via rvm

--- test/lint.sh
+++ test/lint.sh
@@ -2,7 +2,7 @@
 set -o errexit -o noclobber -o nounset -o xtrace
 
 dir=/vagrant/test
-export GEM_PATH="$(gem env gempath | tr ':' '\n' | sed 's#$#/bin#' | tr '\n' ':')"
+export GEM_PATH="$(rvm gem env gempath | tr ':' '\n' | sed 's#$#/bin#' | tr '\n' ':')"
 export PATH="${GEM_PATH}/bin:$PATH"
 
 "${dir}/puppet-lint.sh"
