c8f8377312c6bd002f501837f73bb85632518a77
Victor Engmark <victor.engmark@gmail.com>
2016-06-11 16:50:49 +0100

    Fix VirtualBox guest modules

--- test/setup-dependencies.sh
+++ test/setup-dependencies.sh
@@ -2,8 +2,9 @@
 set -o errexit -o xtrace
 pacman --remove --noconfirm ack bash-completion cloc colordiff cowsay dd_rescue ddrescue dmidecode dstat ethtool \
     figlet fortune-mod gdb glances htop hwinfo inxi lshw lsof nethogs ngrep nmap ponysay pv ranger reptyr sl smem \
-    sshfs stow strace tmux yaourt zsh
+    sshfs stow strace tmux virtualbox-guest-dkms virtualbox-guest-modules virtualbox-guest-utils yaourt zsh
 pacman-key --refresh-keys
+pacman --sync --refresh --noconfirm virtualbox-guest-modules-arch virtualbox-guest-utils
 pacman --sync --refresh --sysupgrade --noconfirm
 pacman-db-upgrade
 pacman --sync --needed --refresh --noconfirm puppet ruby ruby-shadow
