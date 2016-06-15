659466fa0da2ad07ad9213b403f20bd39b56b2eb
Victor Engmark <victor.engmark@gmail.com>
2016-06-15 18:31:08 +0100

    Append /bin to *all* gem path elements

--- test/lint.sh
+++ test/lint.sh
@@ -2,7 +2,7 @@
 set -o errexit -o noclobber -o nounset -o xtrace
 
 dir=/vagrant/test
-PATH="$(gem env gempath | tr ':' '\n' | sed 's#$#/bin#' | tr '\n' ':')/bin:$PATH"
+PATH="$(gem env gempath | tr ':' '\n' | sed 's#$#/bin#' | tr '\n' ':'):$PATH"
 
 export PATH
 
