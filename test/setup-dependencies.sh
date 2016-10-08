9d1090887cb52b1a8d4defe4fbe845df11ef26bc
Victor Engmark <victor.engmark@gmail.com>
2016-10-08 15:18:05 +0100

    Remove reference to package no longer in upstream image

--- test/setup-dependencies.sh
+++ test/setup-dependencies.sh
@@ -2,7 +2,7 @@
 set -o errexit -o xtrace
 pacman --remove --noconfirm ack bash-completion cloc colordiff cowsay dd_rescue ddrescue dmidecode dstat ethtool \
     figlet fortune-mod gdb glances htop hwinfo inxi lshw lsof nethogs ngrep nmap ponysay pv ranger reptyr sl smem \
-    sshfs stow strace tmux virtualbox-guest-dkms virtualbox-guest-modules virtualbox-guest-utils yaourt zsh
+    sshfs stow strace tmux virtualbox-guest-dkms virtualbox-guest-utils yaourt zsh
 pacman-key --refresh-keys
 pacman --sync --refresh --noconfirm virtualbox-guest-modules-arch virtualbox-guest-utils
 pacman --sync --refresh --sysupgrade --noconfirm
