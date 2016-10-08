bb9d30cb0e119496ae6012692786143be38a799b
Victor Engmark <victor.engmark@gmail.com>
2016-10-08 19:23:57 +0100

    Run `archlinux-java set` only when necessary

--- modules/java_development_kit/manifests/init.pp
+++ modules/java_development_kit/manifests/init.pp
@@ -1,6 +1,10 @@
 class java_development_kit {
+  $java_environment_name = 'java-8-openjdk'
+
   package { 'jdk8-openjdk':
     ensure => latest,
   }~>
-  exec { '/usr/bin/archlinux-java set java-8-openjdk': }
+  exec { "/usr/bin/archlinux-java set ${java_environment_name}":
+    unless => "/usr/bin/test \"$(archlinux-java get)\" = '${java_environment_name}'",
+  }
 }
