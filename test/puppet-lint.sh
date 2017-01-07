972f248baee17d0008453890163531037899f2ab
Victor Engmark <victor.engmark@gmail.com>
2017-01-07 18:09:02 +0000

    Simplify lint searches
    Based on <http://stackoverflow.com/a/15736463/96588>.

--- test/puppet-lint.sh
+++ test/puppet-lint.sh
@@ -6,13 +6,9 @@ directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 repository_directory="$(dirname "$directory")"
 
 find "${repository_directory}/manifests" "${repository_directory}/modules" \
-    \( \
-        -path "${repository_directory}/modules/archive" -o \
-        -path "${repository_directory}/modules/idea" -o \
-        -path "${repository_directory}/modules/stdlib" -o \
-        -path "${repository_directory}/modules/ufw" \
-    \) -prune -o \
-    \( \
-        -type f -name '*.pp' \
-        -exec puppet-lint --fail-on-warnings --no-documentation-check {} + \
-    \)
+    -not -path "${repository_directory}/modules/archive/*" \
+    -not -path "${repository_directory}/modules/idea/*" \
+    -not -path "${repository_directory}/modules/stdlib/*" \
+    -not -path "${repository_directory}/modules/ufw/*" \
+    -type f -name '*.pp' \
+    -exec puppet-lint --fail-on-warnings --no-documentation-check {} +
--- test/reek.sh
+++ test/reek.sh
@@ -6,13 +6,9 @@ directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 repository_directory="$(dirname "$directory")"
 
 find "${repository_directory}" \
-    \( \
-        -path "${repository_directory}/modules/archive" -o \
-        -path "${repository_directory}/modules/idea" -o \
-        -path "${repository_directory}/modules/stdlib" -o \
-        -path "${repository_directory}/modules/ufw" \
-    \) -prune -o \
-    \( \
-        -type f \( -name '*.rb' -o -name Vagrantfile \) \
-        -exec reek {} + \
-    \)
+    -not -path "${repository_directory}/modules/archive/*" \
+    -not -path "${repository_directory}/modules/idea/*" \
+    -not -path "${repository_directory}/modules/stdlib/*" \
+    -not -path "${repository_directory}/modules/ufw/*" \
+    -type f \( -name '*.rb' -o -name Vagrantfile \) \
+    -exec reek {} +
