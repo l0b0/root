d5d850788f474af913bdd88f4fc49024073736dc
Victor Engmark <victor.engmark@gmail.com>
2017-01-10 21:34:54 +0000

    Don't configure Rust for root
    Not much use for development.

--- modules/rust/manifests/init.pp
+++ modules/rust/manifests/init.pp
@@ -1,12 +1,5 @@
 class rust {
   package { 'rustup':
     ensure => latest,
-  } ~>
-  exec { [
-    '/usr/bin/rustup toolchain install stable',
-    '/usr/bin/rustup default stable',
-    '/usr/bin/rustup component add rust-src'
-  ]:
-    refreshonly => true,
   }
 }
