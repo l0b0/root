3b18f4b0846972cd07a90370c12a31e2cfbc550f
Victor Engmark <victor.engmark@gmail.com>
2017-03-04 22:00:17 +0000

    Log requests to port 80

--- modules/browser/manifests/init.pp
+++ modules/browser/manifests/init.pp
@@ -5,7 +5,16 @@ class browser {
     ensure => latest,
   }
 
-  firewall { '100 drop insecure outgoing HTTP traffic':
+  firewall { '100 log insecure outgoing HTTP traffic':
+    chain      => 'OUTPUT',
+    dport      => 80,
+    proto      => tcp,
+    jump       => 'LOG',
+    log_level  => debug,
+    log_prefix => 'outgoing HTTP traffic ',
+    log_uid    => true,
+  } ->
+  firewall { '101 drop insecure outgoing HTTP traffic':
     chain  => 'OUTPUT',
     dport  => 80,
     proto  => tcp,
