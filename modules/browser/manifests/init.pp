e3f45cca82c80afb1bf2971276a20be85b5daa7a
Victor Engmark <victor.engmark@gmail.com>
2017-09-20 12:25:27 +0100

    Allow outgoing HTTP traffic to known OCSP responders

--- modules/browser/manifests/init.pp
+++ modules/browser/manifests/init.pp
@@ -5,7 +5,30 @@ class browser {
     ensure => latest,
   }
 
-  firewall { '100 log insecure outgoing HTTP traffic':
+  $firewall_index_start = 100
+  $ocsp_hosts = [
+    'ocsp.comodoca.com',
+    'ocsp.digicert.com',
+    'ocsp.int-x3.letsencrypt.org',
+    'ocsp.msocsp.com',
+    'ocsp.sca1b.amazontrust.com',
+    'ocsp.usertrust.com',
+    'ocsp2.globalsign.com',
+  ]
+
+  $ocsp_host_count = size($ocsp_hosts)
+
+  $ocsp_hosts.each |Integer $index, String $host| {
+    firewall { join([$firewall_index_start + $index, 'allow outgoing HTTP traffic to OCSP responders'], ' '):
+      chain       => 'OUTPUT',
+      destination => $host,
+      dport       => 80,
+      proto       => tcp,
+      action      => accept,
+    }
+  }
+
+  firewall { join([$firewall_index_start + $ocsp_host_count, 'log insecure outgoing HTTP traffic'], ' '):
     chain      => 'OUTPUT',
     dport      => 80,
     proto      => tcp,
@@ -13,7 +36,7 @@ class browser {
     log_level  => debug,
     log_prefix => 'outgoing HTTP traffic ',
     log_uid    => true,
-  } -> firewall { '101 drop insecure outgoing HTTP traffic':
+  } -> firewall { join([$firewall_index_start + $ocsp_host_count + 1, 'drop insecure outgoing HTTP traffic'], ' '):
     chain  => 'OUTPUT',
     dport  => 80,
     proto  => tcp,
