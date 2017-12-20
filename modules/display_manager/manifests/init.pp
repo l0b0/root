67c5d97463da6d26455b63e3080089d0e9cb56b7
Victor Engmark <victorengmark@catalyst.net.nz>
2017-12-20 13:03:15 +1300

    Install default LightDM configuration files

--- modules/display_manager/manifests/init.pp
+++ modules/display_manager/manifests/init.pp
@@ -1,9 +1,27 @@
 class display_manager {
   include display_server
 
+  File {
+    ensure => present,
+    owner  => lightdm,
+    group  => lightdm,
+    mode   => '0644',
+  }
+
   package { ['lightdm', 'lightdm-gtk-greeter', 'lightdm-gtk-greeter-settings']:
     ensure => latest,
-  } -> service { 'lightdm':
-    enable => true,
+  } -> file {
+    '/etc/lightdm/lightdm.conf':
+      source => "puppet:///modules/${module_name}/lightdm.conf";
+    '/etc/lightdm/lightdm-gtk-greeter.conf':
+      source => "puppet:///modules/${module_name}/lightdm-gtk-greeter.conf";
+  }
+
+  service { 'lightdm':
+    enable  => true,
+    require => [
+      Package['lightdm', 'lightdm-gtk-greeter'],
+      File['/etc/lightdm/lightdm.conf', '/etc/lightdm/lightdm-gtk-greeter.conf'],
+    ],
   }
 }
