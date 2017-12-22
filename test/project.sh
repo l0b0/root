3827abfe51fd2b4455d643eb449df90aa2b4ece8
Victor Engmark <victorengmark@catalyst.net.nz>
2017-12-22 13:21:40 +1300

    Fix globbing

--- test/project.sh
+++ test/project.sh
@@ -4,7 +4,7 @@ set -o errexit -o noclobber -o nounset
 directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 repository_directory="$(dirname "$directory")"
 
-for module_path in "${repository_directory}/modules/"
+for module_path in "${repository_directory}/modules/"*
 do
     module_name="$(basename "$module_path")"
     if ! [[ -e "${directory}/modules/$(basename "$module_path")/test.sh" ]]
