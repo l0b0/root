ef06ed04b44a88500c8404296e1e7e5d238642f5
Victor Engmark <victor.engmark@gmail.com>
2018-01-05 20:51:30 +1300

    Install default PulseAudio configuration

--- modules/bluetooth_audio/manifests/init.pp
+++ modules/bluetooth_audio/manifests/init.pp
@@ -4,4 +4,10 @@ class bluetooth_audio {
   package { ['pulseaudio-bluetooth']:
     ensure => latest,
   }
+
+  file { '/etc/pulse/default.pa':
+    source => "puppet:///modules/${module_name}/default.pa",
+  }
+
+  warning('Please restart PulseAudio after any changes to the packages or configuration.')
 }
