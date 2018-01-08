acda702d2270716510bab1d291b66f87fb0318eb
Victor Engmark <victor.engmark@gmail.com>
2018-01-08 19:01:43 +1300

    Make PulseAudio configuration world readable
    It's a user service after all.

--- modules/bluetooth_audio/manifests/init.pp
+++ modules/bluetooth_audio/manifests/init.pp
@@ -7,6 +7,7 @@ class bluetooth_audio {
 
   file { '/etc/pulse/default.pa':
     source => "puppet:///modules/${module_name}/default.pa",
+    mode   => '0644',
   }
 
   warning('Please restart PulseAudio after any changes to the packages or configuration.')
