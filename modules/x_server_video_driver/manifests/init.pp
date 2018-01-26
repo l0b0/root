cbb23bbd5dc60853eb955601298c2c5eee84aa79
Victor Engmark <victor.engmark@gmail.com>
2018-01-26 19:34:55 +1300

    Install nvidia proprietary graphics drivers

--- modules/x_server_video_driver/manifests/init.pp
+++ modules/x_server_video_driver/manifests/init.pp
@@ -1,5 +1,6 @@
 class x_server_video_driver (
-  $intel = false,
+  $intel  = false,
+  $nvidia = false,
 ) {
   require package_manager
 
@@ -26,5 +27,37 @@ class x_server_video_driver (
     ensure => $intel_file_ensure,
     source => "puppet:///modules/${module_name}/intel.conf",
     mode   => '0644',
+  }
+
+  case str2bool($nvidia) {
+    true: {
+      $nvidia_package_ensure = latest
+      $nvidia_file_ensure = present
+    }
+    default: {
+      $nvidia_package_ensure = absent
+      $nvidia_file_ensure = absent
+    }
+  }
+  $nvidia_driver_package = 'nvidia-dkms'
+
+  package { [$nvidia_driver_package, 'lib32-nvidia-utils', 'libvdpau', 'nvidia-settings', 'nvidia-utils']:
+    ensure => $nvidia_package_ensure,
+  }
+
+  file { '/etc/modprobe.d/50-nvidia.conf':
+    ensure => $nvidia_file_ensure,
+    source => "puppet:///modules/${module_name}/nvidia.conf",
+    mode   => '0644',
+  }
+
+  file { '/etc/pacman.d/hooks':
+    ensure => directory,
+    mode   => '0755',
+  } ->
+  file { '/etc/pacman.d/hooks/nvidia.hook':
+    ensure  => $nvidia_file_ensure,
+    content => template("${module_name}/nvidia.hook.erb"),
+    mode    => '0644',
   }
 }
