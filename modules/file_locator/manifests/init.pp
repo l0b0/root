2f4e5cc2f02ffd6a99fef429df6ec27ee94f056c
Victor Engmark <victorengmark@catalyst.net.nz>
2018-01-26 15:51:31 +1300

    Set up an updatedb cron job
    Fixes issue #16.

--- modules/file_locator/manifests/init.pp
+++ modules/file_locator/manifests/init.pp
@@ -3,5 +3,9 @@ class file_locator {
 
   package { 'mlocate':
     ensure => latest,
+  } -> cron { 'updatedb':
+    command => '/usr/bin/updatedb',
+    hour    => fqdn_rand(24),
+    minute  => fqdn_rand(60),
   }
 }
