6167b13c7b36dfc2cd7fa2bac90578980225a41d
Victor Engmark <victor.engmark@gmail.com>
2018-02-24 13:07:51 +1300

    Upgrade to JDK 9

--- modules/java_development_kit/manifests/init.pp
+++ modules/java_development_kit/manifests/init.pp
@@ -1,7 +1,7 @@
 class java_development_kit {
-  $java_environment_name = 'java-8-openjdk'
+  $java_environment_name = 'java-9-openjdk'
 
-  package { 'jdk8-openjdk':
+  package { 'jdk9-openjdk':
     ensure => installed,
   } ~> exec { "/usr/bin/archlinux-java set ${java_environment_name}":
     unless => "/usr/bin/test \"$(archlinux-java get)\" = '${java_environment_name}'",
