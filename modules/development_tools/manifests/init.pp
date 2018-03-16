447662891989e77035a6c42b0431862672a60b43
Victor Engmark <victor.engmark@gmail.com>
2018-03-17 08:55:34 +1300

    Install GDB
    Resolves #61.

--- modules/development_tools/manifests/init.pp
+++ modules/development_tools/manifests/init.pp
@@ -1,7 +1,7 @@
 class development_tools {
   include shell
 
-  package { ['base-devel', 'cmake']:
+  package { ['base-devel', 'cmake', 'gdb']:
     ensure        => installed,
     allow_virtual => true,
   }
