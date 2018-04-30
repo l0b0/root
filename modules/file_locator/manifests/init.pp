fb23670b512613eeac56e3cbff1c5784e50ecf6d
Victor Engmark <victor.engmark@gmail.com>
2018-04-30 20:07:45 +1200

    Seed random generator with current module
    Avoids all cron jobs running at the same time.

--- modules/file_locator/manifests/init.pp
+++ modules/file_locator/manifests/init.pp
@@ -5,7 +5,7 @@ class file_locator {
     ensure => installed,
   } -> cron { 'updatedb':
     command => '/usr/bin/updatedb',
-    hour    => fqdn_rand(24),
-    minute  => fqdn_rand(60),
+    hour    => fqdn_rand(24, $module_name),
+    minute  => fqdn_rand(60, $module_name),
   }
 }
